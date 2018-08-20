47b497cc3c630074219440cd6c825edf1a1c07ee
nidhoggfgg <39847056+nidhoggfgg@users.noreply.github.com>
2018-08-20 18:15:05 +0800

    Add files via upload

--- tools.sh
+++ tools.sh
@@ -36,7 +36,7 @@ qt(){
 		;;
 		2) stcs
 		;;
-		3) echo "deb http://mirrors.tuna.tsinghua.edu.cn/termux stable main" > $PREFIX/etc/apt/sources.list
+		3) cd&&echo "deb http://mirrors.tuna.tsinghua.edu.cn/termux stable main" > $PREFIX/etc/apt/sources.list&&qt
 		;;
 		4) pyk
 		;;
@@ -44,7 +44,7 @@ qt(){
 		;;
 		6) pkg install curl tsu proot wget vim unzip unrar -y&&qt
 		;;
-		7) echo "set fileencodings=utf-8,gb2312,gb18030,gbk,ucs-bom,cp936,latin1" >. vimrc&&echo "set enc=utf8" >>. vimrc&&echo set "fencs=utf8,gbk,gb2312,gb18030" >>. vimrc&&source. vimrc&&qt
+		7) cd&&echo "set fileencodings=utf-8,gb2312,gb18030,gbk,ucs-bom,cp936,latin1" >. vimrc&&echo "set enc=utf8" >>. vimrc&&echo set "fencs=utf8,gbk,gb2312,gb18030" >>. vimrc&&source. vimrc&&qt
 		66) echo "感谢使用"
 		;;
 esac
