3920d624bd292eb79b874dd4c8d921d7c72f973e
tsvico <tsxygwj@163.com>
2019-02-05 14:20:56 +0800

    pull原分支

--- all_python_libs.sh
+++ all_python_libs.sh
@@ -1,3 +1,4 @@
+bash repo.sh
 apt install libxml2 libxml2-dev libxslt libxslt-dev openssl libffi libffi-dev openssl-tool openssl-dev fftw fftw-dev libzmq libzmq-dev freetype freetype-dev libpng libpng-dev pkg-config scrypt -y
 pkg install libcrypt libcrypt-dev ccrypt libgcrypt libgcrypt-dev-y
 pkg install python python-dev ndk-sysroot clang make libjpeg-turbo-dev -y
